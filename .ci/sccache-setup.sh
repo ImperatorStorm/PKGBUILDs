3f6119c5dd3be08358056e1e91d987ae8b40564d
ImperatorStorm <imperatorstorm11@protonmail.com>
2026-05-24 18:33:19 -0500

    Really hacky `sccache` GHA cache implementation

---
+++ .ci/sccache-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+install -vdm 755 "/usr/lib/sccache/bin"
+local _prog
+for _prog in gcc g++ c++; do
+  ln -vs /usr/bin/sccache "/usr/lib/sccache/bin/$_prog"
+  ln -vs /usr/bin/sccache "/usr/lib/sccache/bin/$(uname -m)-$_prog"
+done
+# sccache supports rustc here too
+for _prog in cc clang clang++ rustc; do
+  ln -vs /usr/bin/sccache "/usr/lib/sccache/bin/$_prog"
+done
