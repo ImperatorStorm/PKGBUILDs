7a4bbc2511fd6b0c6d8632075c6f4efeca2b1969
ImperatorStorm <imperatorstorm11@protonmail.com>
2026-05-25 15:35:54 -0500

    Use action-provided `sccache`

--- .ci/sccache-setup.sh
+++ .ci/sccache-setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 install -vdm 755 "/usr/local/bin"
 for _prog in gcc g++ c++; do
-  ln -vs /usr/bin/sccache "/usr/local/bin/$_prog"
-  ln -vs /usr/bin/sccache "/usr/local/bin/$(uname -m)-$_prog"
+  ln -vs ${SCCACHE_PATH} "/usr/local/bin/$_prog"
+  ln -vs ${SCCACHE_PATH} "/usr/local/bin/$(uname -m)-$_prog"
 done
 # sccache supports rustc here too
 for _prog in cc clang clang++ rustc; do
-  ln -vs /usr/bin/sccache "/usr/local/bin/$_prog"
+  ln -vs ${SCCACHE_PATH} "/usr/local/bin/$_prog"
 done
